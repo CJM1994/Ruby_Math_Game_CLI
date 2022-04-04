7735003dab27a62c6edffdc67fb1873c572d22a1
CJM1994 <connorjohnmullin@gmail.com>
2022-04-04 13:08:24 -0700

    Add player wins line

--- main.rb
+++ main.rb
@@ -12,16 +12,17 @@ player_two = Player.new(gets.chomp)
 
 player_turn = PLAYER_ONE
 
-def start_match
-  match = Match.new
+match = Match.new
 
-  while match.over == false
-    correct_answer = Question.new.run_question(player_turn)
+while match.over == false
+  correct_answer = Question.new.run_question(player_turn)
 
-    match.add_point(player_turn) if correct_answer == true
+  match.add_point(player_turn) if correct_answer == true
 
-    match.display_scoreboard
-    player_turn = (player_turn == PLAYER_ONE ? PLAYER_TWO : PLAYER_ONE)
-    puts '----- NEW TURN -----' unless match.over == true
-  end
+  match.display_scoreboard
+  player_turn = (player_turn == PLAYER_ONE ? PLAYER_TWO : PLAYER_ONE)
+  puts '----- NEW TURN -----' unless match.over == true
 end
+
+winner = player_turn == PLAYER_ONE ? player_two.name : player_one.name
+puts "GAME OVER: #{winner} Wins!!!"
